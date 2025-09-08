5e74be418fea969634ee4b068435ef0b3fbd2feb
thisdev-davi <davisouzaoliveira1975@gmail.com>
2025-09-08 13:07:50 -0300

    MainPilha

--- collections/stack/MainPilha.java
+++ collections/stack/MainPilha.java
@@ -1,43 +1,94 @@
-package collections.stack;
+package trabalho;
 import java.util.Scanner;
 
-public class MainPilha {
+public class Main {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        PilhaInt stack = new PilhaInt();
-        preencher(stack);
-        if (stack.size() > 0){
-        System.out.println("Topo da Pilha: " + stack.top());
-        }
-        remover(stack);
+        PilhaPaginas stack = new PilhaPaginas();
 
-    }
+        int escolha;
+        do {
+            System.out.print("\n-- Menu --" +
+                    "\n1- Acessar nova página"
+                    + "\n2- Voltar para página Anterior"
+                    + "\n3- Ver página atual"
+                    + "\n4- Exibir histórico de navegação"
+                    + "\n5- Sair" +
+                    "\n>> ");
+            escolha = sc.nextInt();
+            sc.nextLine();
 
-    static void preencher(PilhaInt stack){
-        System.out.println("-- Preencher Lista --");
-        int num = -1;
-        while(num != 0) {
-            System.out.print("Digite um num (0 - sair): ");
-            num = sc.nextInt();
-            if (num!=0){
-                stack.push(num);
+            switch (escolha) {
+                case 1:
+                    novaPagina(stack);
+                    break;
+                case 2:
+                    voltarPagina(stack);
+                    break;
+                case 3:
+                	paginaAtual(stack);
+                	break;
+                case 4:
+                	historico(stack);
+                    break;
+                case 5:
+                	System.out.println("Até logo :)");
+                default:
+                    System.out.println("Opção inválida!");
             }
-            System.out.println(stack.top());
-        }
+        } while (escolha != 5);
     }
 
-    static void remover(PilhaInt stack){
-        System.out.println("-- Remover Lista --");
-        if(stack.isEmpty()){
-            System.out.println("Lista vazia!");
-            return;
+    static void novaPagina(PilhaPaginas stack){
+        System.out.println("\n-- Acessar Nova Página --");
+        
+        String url = "";
+        String titulo = "";
+        while(url.isEmpty()) {
+        	System.out.print("Url: ");
+            url =  sc.nextLine();	
         }
+        while(titulo.isEmpty()) {
+        	System.out.print("Titulo: ");
+            titulo = sc.nextLine();	
+        }
+        stack.push(new Pagina(url,titulo));
+    }
+    
+    static void voltarPagina(PilhaPaginas stack) {
+    	if((stack.isEmpty()) || stack.size()==1) {
+    		System.out.println("Nenhuma Página Disponível!");
+    		return;
+    	}
+    	stack.pop();
+    	paginaAtual(stack);
+    }
 
-        String removidos = "";
+    static void paginaAtual(PilhaPaginas stack) {
+    	if(stack.isEmpty()) {
+    		System.out.println("Nenhuma Pagina Acessada!");
+    		return;
+    	}
+    	System.out.println("\n-- Página Atual --");
+    	System.out.println(stack.top());
+    }
+    
+    static void historico(PilhaPaginas stack) {
+    	if(stack.isEmpty()) {
+    		System.out.println("Nenhuma Pagina Acessada!");
+    		return;
+    	}
+    	
+        PilhaPaginas aux = new PilhaPaginas();
+        System.out.println("\n-- Histórico --");
         while(!stack.isEmpty()){
-            removidos += " " + stack.pop();
+            System.out.println(stack.top());
+            aux.push(stack.pop());
         }
-        System.out.println("Elementos Removidos: " + removidos);
-    }
+
+        while(!aux.isEmpty()){
+            stack.push(aux.pop());
+        }
+    }   
 }
