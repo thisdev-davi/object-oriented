dcdcf3206acb276cd7876f0cb0fc94710b2916e8
thisdev-davi <davisouzaoliveira1975@gmail.com>
2025-10-09 16:34:51 -0300

    method override

--- reutilizacao/animais/Gato.java
+++ reutilizacao/animais/Gato.java
@@ -4,7 +4,8 @@ public class Gato extends Animal {
 	public Gato(String nome, String raca) {
 		super(nome, raca);
 	}
-	
+
+	// metodo abstrato
 	@Override
 	public String caminha() {
 		return super.getNome() + " está saltando.";
